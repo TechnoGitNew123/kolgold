ead85c8a336c3f3a970d1d876b06cf1aa7ad3133
technothinksup <projects.technothinksup@gmail.com>
2020-06-11 18:03:07 +0530

    footer link

--- application/controllers/Website.php
+++ application/controllers/Website.php
@@ -13,6 +13,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       $this->load->view('Website/footer');
     }
 
+    public function privacy_policy(){
+      $this->load->view('Website/header');
+      $this->load->view('Website/privacy_policy');
+      $this->load->view('Website/footer');
+    }
+
+    public function shipping_policy(){
+      $this->load->view('Website/header');
+      $this->load->view('Website/shipping_policy');
+      $this->load->view('Website/footer');
+    }
+
+    public function cancel_policy(){
+      $this->load->view('Website/header');
+      $this->load->view('Website/cancel_policy');
+      $this->load->view('Website/footer');
+    }
+
+    public function return_policy(){
+      $this->load->view('Website/header');
+      $this->load->view('Website/return_policy');
+      $this->load->view('Website/footer');
+    }
+
+    public function terms(){
+      $this->load->view('Website/header');
+      $this->load->view('Website/terms');
+      $this->load->view('Website/footer');
+    }
+
+    public function disclaimer(){
+      $this->load->view('Website/header');
+      $this->load->view('Website/disclaimer');
+      $this->load->view('Website/footer');
+    }
+
+    public function payment_methode(){
+      $this->load->view('Website/header');
+      $this->load->view('Website/payment_methode');
+      $this->load->view('Website/footer');
+    }
+
     public function login(){
       $this->load->view('Website/login');
     }
---
+++ application/views/Website/disclaimer.php
@@ -0,0 +1,26 @@
+<section class="default-page-heading">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2>Disclaimer</h2>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="default-page">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+
+        <p>The data contained in this site is for general data purposes allegorically. The data is given by Kalapuri.com recalling that we attempt to stay up with the latest and right, we make no representations or affirmations of any sort, express or recommended, about the completeness, exactness, immovable quality, appropriateness or accessibility concerning the site or the data, things, benefits, or related arrangement contained on the site for any reason. Any dependency you put on such data is along these lines by and large at your own particular risk.</p>
+
+        <p>In no occasion,we will be committed for any misfortune or harm including without hindrance, roaming or gigantic hardship or hurt, any difficulty or wickedness at all ascending out of loss of information or points of interest creating out of, or as to, the utilization of this site.</p>
+
+        <p>Through this site you can relationship with different areas which are not under the control of Kalapuri.com. We have no effect over the nature, substance and accessibility of those objectives. The possibility of any affiliations does not by any extend of the creative energy propose a recommendation or handle the perspective communicated inside them.</p>
+
+        <p>Each exertion is made to keep the site up and running easily. All things considered, Kalapuri.com acknowledge no commitment for, and won't be at risk for, the site being by chance closed off in perspective of specific issues outside our ability to control.</p>
+  </div>
+    </div>
+  </div>
+</section>
